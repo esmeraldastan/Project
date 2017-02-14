d90054c9ee49091e0c7de4e29c8bd7b6bd5c2c37
Chase <esmeraldarekz99@gmail.com>
2017-02-14 12:56:07 -0800

    save

--- 021017/Items.java
+++ 021017/Items.java
@@ -2,23 +2,13 @@
 /**
  * Abstract class Items - write a description of the class here
  * 
- * @author (your name here)
- * @version (version number or date here)
+ * @author Esmeralda Lopez
+ * @version 2.14.17
  */
-public abstract class Items
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y    a sample parameter for a method
-     * @return        the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+public abstract class Items{
+    char name;
+    public Items
+   
+    
+   
 }
---
+++ 021017/Weapons.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Weapons here.
+ * 
+ * @author Esmeralda Lopez
+ * @version 2.14.17
+ */
+public class Weapons extends Items{
+   
+
+}
